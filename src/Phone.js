e7d9f959c0afe054e2adaa23c3cd900c98d8eba8
student-monika123 <152508212+student-monika123@users.noreply.github.com>
2024-03-05 10:27:55 +0530

    first commit

---
+++ src/Phone.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function Phone() {
+  return (
+    <>
+    <div className='App' style={{padding: "85px"}}>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-4'>
+                <div className="field--type-link"><a className="icon-link" href="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/c/s/x/-original-imagzjhwaaewgj8r.jpeg?q=70" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/4/h/e/-original-imagzjhwmsamexfk.jpeg?q=70"
+          alt="example"
+        /></span></a></div>
+
+                </div>
+                <div className='col-md-4'>
+                    {/* /* <a href="/">vivo T2x 5G (Glimmer Black, 128 GB)  (6 GB RAM)</a> */}
+                    {/* <a href="/">4.43,60,889 Ratings & 20,325 Reviews</a>  */}
+                    <div class="_4rR01T"><b>vivo T2x 5G (Glimmer Black, 128 GB)</b></div>
+                    <div class="gUuXy-"><span id="productRating_LSTMOBGZBFUF389GDMPYKMTEO_MOBGZBFUF389GDMP_" class="_1lRcqv"><div class="_3LWZlK"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMyIgaGVpZ2h0PSIxMiI+PHBhdGggZmlsbD0iI0ZGRiIgZD0iTTYuNSA5LjQzOWwtMy42NzQgMi4yMy45NC00LjI2LTMuMjEtMi44ODMgNC4yNTQtLjQwNEw2LjUuMTEybDEuNjkgNC4wMSA0LjI1NC40MDQtMy4yMSAyLjg4Mi45NCA0LjI2eiIvPjwvc3ZnPg==" class="_1wB99o"/></div></span><span class="_2_R_DZ"><span><span>3,60,534 Ratings&nbsp;</span><span class="_13vcmD">&amp;</span><span>&nbsp;20,298 Reviews</span></span></span></div><div class="fMghEO"><ul class="_1xgFaf"><li class="rgWa7D">6 GB RAM | 128 GB ROM</li><li class="rgWa7D">16.71 cm (6.58 inch) Full HD+ Display</li><li class="rgWa7D">50MP + 2MP | 8MP Front Camera</li><li class="rgWa7D">5000 mAh Battery</li><li class="rgWa7D">Mediatek Dimensity 6020 Processor</li><li class="rgWa7D">1 Year of Device &amp; 6 Months for Inbox Accessories</li></ul></div></div>
+                    
+                
+                <div className='col-md-4'>
+                <div class="col col-5-12 nlI3QM"><div class="_3tbKJL"><div class="_25b18c"><div class="_30jeq3 _1_WHN1" style={{fontSize:"36px"}}>₹12,999</div><div class="_3I9_wc _27UcVY"><del>₹18,999</del></div><div class="_3Ay6Sb"><span>31% off</span></div></div></div></div>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+
+
+    <div className='App' style={{padding: "85px"}}>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-4'>
+                <div className="field--type-link"><a className="icon-link" href="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/c/s/x/-original-imagzjhwaaewgj8r.jpeg?q=70" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/4/h/e/-original-imagzjhwmsamexfk.jpeg?q=70"
+          alt="example"
+        /></span></a></div>
+
+                </div>
+                <div className='col-md-4'>
+                    {/* /* <a href="/">vivo T2x 5G (Glimmer Black, 128 GB)  (6 GB RAM)</a> */}
+                    {/* <a href="/">4.43,60,889 Ratings & 20,325 Reviews</a>  */}
+                    <div class="_4rR01T"><b>vivo T2x 5G (Glimmer Black, 128 GB)</b></div>
+                    <div class="gUuXy-"><span id="productRating_LSTMOBGZBFUF389GDMPYKMTEO_MOBGZBFUF389GDMP_" class="_1lRcqv"><div class="_3LWZlK"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMyIgaGVpZ2h0PSIxMiI+PHBhdGggZmlsbD0iI0ZGRiIgZD0iTTYuNSA5LjQzOWwtMy42NzQgMi4yMy45NC00LjI2LTMuMjEtMi44ODMgNC4yNTQtLjQwNEw2LjUuMTEybDEuNjkgNC4wMSA0LjI1NC40MDQtMy4yMSAyLjg4Mi45NCA0LjI2eiIvPjwvc3ZnPg==" class="_1wB99o"/></div></span><span class="_2_R_DZ"><span><span>3,60,534 Ratings&nbsp;</span><span class="_13vcmD">&amp;</span><span>&nbsp;20,298 Reviews</span></span></span></div><div class="fMghEO"><ul class="_1xgFaf"><li class="rgWa7D">6 GB RAM | 128 GB ROM</li><li class="rgWa7D">16.71 cm (6.58 inch) Full HD+ Display</li><li class="rgWa7D">50MP + 2MP | 8MP Front Camera</li><li class="rgWa7D">5000 mAh Battery</li><li class="rgWa7D">Mediatek Dimensity 6020 Processor</li><li class="rgWa7D">1 Year of Device &amp; 6 Months for Inbox Accessories</li></ul></div></div>
+                    
+                
+                <div className='col-md-4'>
+                <div class="col col-5-12 nlI3QM"><div class="_3tbKJL"><div class="_25b18c"><div class="_30jeq3 _1_WHN1" style={{fontSize:"36px"}}>₹12,999</div><div class="_3I9_wc _27UcVY"><del>₹18,999</del></div><div class="_3Ay6Sb"><span>31% off</span></div></div></div></div>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+    </>
+  )
+}
---
+++ src/components/Product.js
@@ -0,0 +1,132 @@
+import React from 'react'
+import './Product.css';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+
+export default function Product() {
+  return (
+    <>
+    
+
+  <div >
+    <CardGroup >
+      <Card>
+        <Card.Body >
+          <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/057ba6c2c95f3d22.jpg?q=80"
+          alt="example"
+        /></span></a></div>
+        </Card.Body> 
+      </Card>
+      <Card>
+        <Card.Body>
+        <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/81035a28051d094e.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+        <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/2ad216676e96d294.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body>       
+      </Card>
+    </CardGroup>
+
+
+
+    <CardGroup>
+      <Card>
+        <Card.Body>
+          <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/9926b000d9e06f79.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body> 
+      </Card>
+      <Card>
+        <Card.Body>
+        <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/3dbceec64ee01bdc.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+        <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/6f23fc1a646a9c64.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body>       
+      </Card>
+    </CardGroup>
+
+
+    <CardGroup>
+      <Card>
+        <Card.Body>
+          <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/30527c6a71197cda.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body> 
+      </Card>
+      <Card>
+        <Card.Body>
+        <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/fe42cb569b6a602a.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+        <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/8c847eea35140ed6.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body>       
+      </Card>
+    </CardGroup>
+
+
+
+    <CardGroup>
+      <Card>
+        <Card.Body>
+          <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/ae3a176640c25f34.jpeg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body> 
+      </Card>
+      <Card>
+        <Card.Body>
+        <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/b542d3cf44569508.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+        <div className="field--type-link"><a className="icon-link" href="http://localhost:3002/" target="" title="Search jobs"><span className="wrapped-link-text"><img
+          src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/7831c7b7305b24a1.jpg?q=20"
+          alt="example"
+        /></span></a></div>
+        </Card.Body>       
+      </Card>
+    </CardGroup>
+  </div>
+
+ 
+  
+    </>
+    
+  )
+}
